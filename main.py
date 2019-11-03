a290a2724e2f4c2adefee8e57902094c0a1c5cc3
FabianHeinemann <fabian.heinemann@gmx.net>
2019-11-03 15:23:26 +0100

    added csv reading and serialize

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from model import *
 import datetime
 
+
 def main():
     # Create IndexList object
-    list = IndexList("stocks.yaml", datetime.date(2019, 10, 22), datetime.date(2019, 10, 29))
+    index_list = IndexList(datetime.date(2019, 10, 22), datetime.date(2019, 10, 29))
 
-    # Initialize the index list
-    list.initialize_indexes(source="test")
+    # Initialize the index list from source
+    # DEBUG: source = "test"
+    index_list.initialize_indexes(source="test")
 
     # Print output of all available data
-    for i in list.list_indices():
+    for i in index_list.list_indices():
         print(i)
         print(i.quotation_df)
+
+    # Serialize index data to csv
+    index_list.serialize_indexes(target="csv")
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -9,20 +9,31 @@ import requests
 import datetime
 import numpy as np
 
+
 class IndexList:
     """ Class to manage lists of indexes
 
     Parameters:
-        yaml_file (string): yaml file path containing indexes
         start_date (datetime.date): start_date to query
         end_date (datetime.date): end_date to query
         proxies:  proxy dict. Default: {} (no proxy)
+        yaml_file(string, Default: stocks.yaml): yaml containing dict of indexes and names.
 
     """
 
-    def __init__(self, yaml_file, start_date, end_date, proxies = {}):
-        # csv to store stock data (optional)
+    def __init__(self, start_date, end_date, proxies=None, yaml_file="stocks.yaml"):
+        # csv file name to store stock data (optional)
+        if proxies is None:
+            proxies = {}
         self.csv_file = "stock_data.csv"
+
+        # yaml_file to read indexes dict from
+        self.yaml_file = yaml_file
+
+        # Read index_dict from yaml
+        self.index_dict = {}
+        if self.yaml_file:
+            self.index_dict = self.read_index_list_from_yaml()
 
         #
         self.start_date = start_date
@@ -32,24 +43,33 @@ class IndexList:
         self.indices = []
 
         self.session = None
-        if (proxies):
+        if proxies:
             # Create the session and set the proxies.
             self.session = requests.Session()
             self.session.proxies.update(proxies)
 
-        # Read indexes from yaml
-        self.index_dict = {}
-        with open(yaml_file, 'r') as yaml_file:
-            try:
-                self.index_dict = yaml.safe_load(yaml_file)["indexes"]
-            except yaml.YAMLError as exc:
-                print(exc)
-
     def list_indices(self):
         return self.indices
 
+    def read_index_list_from_yaml(self):
+        """ Reads index list from yaml. Will populate self.index list.
+
+        returns:
+            index_dict: Dictionary of index_id - index_name pairs
+
+        """
+        # Read indexes from yaml
+        index_dict = {}
+        with open(self.yaml_file, 'r') as f:
+            try:
+                index_dict = yaml.safe_load(f)["indexes"]
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        return index_dict
+
     def initialize_indexes(self, source="web"):
-        """ Starts a full update of all indexes from the sources
+        """ Performs a full update of all indexes from the sources
             Times are defined by self.start_date and self.end_date
         Parameter:
             source (string): Selects source
@@ -63,22 +83,44 @@ class IndexList:
         """
         if source == "web":
             # Read from web
+            print("Read from web")
             for symbol in self.index_dict:
                 print(symbol)
                 stooq_data = self.get_stooq_data(symbol)
-                if not(stooq_data.empty):
+                if not stooq_data.empty:
+                    # Create new index object
                     new_index = Index(self.index_dict.get(symbol), symbol, stooq_data.index[-1], stooq_data.index[0])
+                    # Add dataframe with time series data
                     new_index.set_quotations(stooq_data)
                     self.indices.append(new_index)
                 else:
                     print("Read error.")
         elif source == "csv":
             # read from csv file
-            # tbd
-            pass
+            print("Read from csv: %s" % (self.csv_file))
+
+            # Read csv
+            indexes_dataframe = pd.read_csv(self.csv_file, parse_dates=["Date"])
+
+            # Create index dict and index object from data in csv
+            self.index_dict = {}
+            for name in indexes_dataframe["Name"].unique():
+                symbol = indexes_dataframe[indexes_dataframe["Name"] == name]["Symbol"].unique()[0]
+                self.index_dict[symbol] = name
+
+                start_time = min(indexes_dataframe[indexes_dataframe["Name"] == name]["Date"])
+                end_time = max(indexes_dataframe[indexes_dataframe["Name"] == name]["Date"])
+
+                # Create new index object
+                new_index = Index(name, symbol, start_time, end_time)
+
+                # Add dataframe with time series data
+                new_index.set_quotations(indexes_dataframe[indexes_dataframe["Name"] == name])
+                self.indices.append(new_index)
+
         elif source == "db":
             # read from database
-            # tbd
+            # to do
             pass
         elif source == "test":
             # generate empty test data
@@ -92,13 +134,36 @@ class IndexList:
                 delta = datetime.timedelta(days=1)
                 current_date = self.start_date
                 while current_date <= self.end_date:
+                    test_data = test_data.append({"Date": current_date, "Quotation": np.random.rand(1)[0], "Symbol": symbol,
+                                                  "Name": self.index_dict.get(symbol)}, ignore_index=True)
                     current_date += delta
-                    test_data = test_data.append({"Date": current_date, "Quotation": np.random.rand(1)[0], "Id" : symbol, "Name" : self.index_dict.get(symbol)}, ignore_index=True)
 
                 new_index.set_quotations(test_data)
                 self.indices.append(new_index)
 
+    def serialize_indexes(self, target="csv"):
+        """ Serializes data to target data store
+        Parameter:
+            target (string): Selects data store
+                    "csv": write to csv file (default)
+                    "db":  write to database
+        Returns:
+            (nothing)
+        """
+        if target == "csv":
+            # Serialize to csv
 
+            # Create a dataframe
+            indexes_dataframe = pd.DataFrame()
+            for i in self.list_indices():
+                indexes_dataframe = indexes_dataframe.append(i.quotation_df, ignore_index=True)
+
+            # Save to disk
+            indexes_dataframe.to_csv(self.csv_file, index=False)
+
+        elif target == "db":
+            # Serialize to database
+            pass
 
     def get_stooq_data(self, symbol):
         """ Returns stooq temporal stock data for a given symbol
@@ -121,6 +186,7 @@ class IndexList:
             session=self.session
         ).read()
 
+
 class Index:
     """ Class to store index data
     Parameters:
@@ -129,6 +195,7 @@ class Index:
         start_date (datetime.date): start date of index
         last_date (datetime.date): last available date of index
     """
+
     def __init__(self, name, symbol, start_date, last_date):
         self.name = name
         self.symbol = symbol
@@ -145,4 +212,4 @@ class Index:
 
     def __str__(self):
         """ String serialization method """
-        return("----- + %s + -----\nfrom: %s to: %s" %  (self.name, self.start_date, self.last_date))
+        return "----- + %s + -----\nfrom: %s to: %s" % (self.name, self.start_date, self.last_date)
